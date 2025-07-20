780aae7f1c7e4011649899962600ddfa1073784a
caotrung <caotrung.kk@gmail.com>
2025-07-20 22:02:48 +0700

    Trigger Github action

--- app/src/main/java/com/trungcs/showcaseproject/MainActivity.kt
+++ app/src/main/java/com/trungcs/showcaseproject/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             ShowcaseProjectTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android With Action",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
